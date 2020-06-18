150cf1cf3d48225b53bbd666dcd4135237d99029
Anton Bakker <anton.bakker@kadaster.nl>
2020-06-18 17:22:03 +0200

    added grayscale background layers and more

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import WMSCapabilities from 'ol/format/WMSCapabilities'
 import { Map, View } from 'ol'
 import WMTSSource from 'ol/source/WMTS'
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
+import { Tile as TileLayer, Image as ImageLayer } from 'ol/layer'
 import WMTSTileGrid from 'ol/tilegrid/WMTS.js'
 import { register } from 'ol/proj/proj4.js'
 import { transform } from 'ol/proj'
-import { Circle as Fill, Stroke, Style } from 'ol/style';
 import proj4 from 'proj4'
 import Projection from 'ol/proj/Projection'
 import { getTopLeft } from 'ol/extent.js'
 import ImageWMS from 'ol/source/ImageWMS'
-import { Image as ImageLayer } from 'ol/layer'
 
 import LayerSwitcher from 'ol-layerswitcher'
 import 'ol/ol.css'
 import 'ol-layerswitcher/src/ol-layerswitcher.css'
+
 var currentLayer
 
 // add map
@@ -23,28 +22,28 @@ const LUFO_ATTRIBUTION = 'Kaartgegevens: © <a href="http://www.kadaster.nl">Kad
 
 proj4.defs('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs')
 register(proj4)
-const rdProjection = new Projection({
+const RDPROJECTION = new Projection({
   code: 'EPSG:28992',
   extent: [-285401.92, 22598.08, 595401.92, 903401.92]
 })
 
-const resolutions = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210]
-const matrixIds = new Array(15)
+const RESOLUTIONS = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210]
+const MATRIXIDS = new Array(15)
 for (var i = 0; i < 15; ++i) {
-  matrixIds[i] = i
+  MATRIXIDS[i] = i
 }
 
-function stripQueryParams(serviceUrl) {
+function stripQueryParams (serviceUrl) {
   const urlObj = new URL(serviceUrl)
   return `${urlObj.protocol}//${urlObj.host}:${urlObj.port}${urlObj.pathname}?`
 }
 
-function getWMSLayer(serviceUrl, serviceTitle, layers, styles) {
-  let layersReversed = layers.reverse()
-  let layersParam = layersReversed.join(",")
-  let stylesReversed = styles.reverse()
-  let stylesParam = stylesReversed.join(",")
-  console.log(layersParam)
+function getWMSLayer (serviceUrl, serviceTitle, layers, styles) {
+  const layersReversed = layers.reverse()
+  const layersParam = layersReversed.join(',')
+  const stylesReversed = styles.reverse()
+  const stylesParam = stylesReversed.join(',')
+
   const wmsSource = new ImageWMS({
     crossOrigin: 'anonymous',
     url: stripQueryParams(serviceUrl),
@@ -52,53 +51,46 @@ function getWMSLayer(serviceUrl, serviceTitle, layers, styles) {
     ratio: 1,
     hidpi: false,
     serverType: 'mapserver',
-    projection: rdProjection
+    projection: RDPROJECTION
   })
   return new ImageLayer({
     visible: true,
-    opacity: document.getElementById("opacitySlider").value/100,
+    opacity: document.getElementById('opacitySlider').value / 100,
     source: wmsSource,
-    // title: serviceTitle,
     crossOrigin: 'anonymous'
   })
 }
 
-
-
-
-
-function switchLayer(serviceUrl, serviceTitle) {
+function switchLayer (serviceUrl, serviceTitle) {
   if (currentLayer) {
-    map.removeLayer(currentLayer)
+    MAP.removeLayer(currentLayer)
   }
-  let layers = []
-
-  let els = document.getElementsByClassName("layerCheck");
-  let styleEls = document.getElementsByClassName("styleselect");
-  let styles = []
+  const layers = []
+  const els = document.getElementsByClassName('layerCheck')
+  const styleEls = document.getElementsByClassName('styleselect')
+  const styles = []
   Array.prototype.forEach.call(els, function (el) {
     if (el.checked) {
-      let currentLayer = el.getAttribute("layer")
+      const currentLayer = el.getAttribute('layer')
       layers.push(currentLayer)
       Array.prototype.forEach.call(styleEls, function (styleEl) {
-        let styleLyrName = styleEl.getAttribute("layer")
-        if (currentLayer===styleLyrName){
+        const styleLyrName = styleEl.getAttribute('layer')
+        if (currentLayer === styleLyrName) {
           styles.push(styleEl.value)
         }
       })
     }
-  });
+  })
   currentLayer = getWMSLayer(serviceUrl, serviceTitle, layers, styles)
-  map.addLayer(currentLayer)
-  map.render()
+  MAP.addLayer(currentLayer)
+  MAP.render()
 }
 
-
-function getWmtsLayer(serviceUrl, attribution, layername) {
+function getWmtsLayer (serviceUrl, attribution, layername) {
   return new TileLayer({
     title: `${layername} WMTS`,
     visible: false,
-    extent: rdProjection.extent,
+    extent: RDPROJECTION.extent,
     source: new WMTSSource({
       crossOrigin: 'anonymous',
       url: serviceUrl,
@@ -106,18 +98,18 @@ function getWmtsLayer(serviceUrl, attribution, layername) {
       matrixSet: 'EPSG:28992',
       format: 'image/png',
       attributions: attribution,
-      projection: rdProjection,
+      projection: RDPROJECTION,
       tileGrid: new WMTSTileGrid({
-        origin: getTopLeft(rdProjection.getExtent()),
-        resolutions: resolutions,
-        matrixIds: matrixIds
+        origin: getTopLeft(RDPROJECTION.getExtent()),
+        resolutions: RESOLUTIONS,
+        matrixIds: MATRIXIDS
       }),
       style: 'default'
     })
   })
 }
 
-function getBRTALayer(layername) {
+function getBRTALayer (layername) {
   return getWmtsLayer('https://geodata.nationaalgeoregister.nl/tiles/service/wmts', BRTA_ATTRIBUTION, layername)
 }
 
@@ -126,18 +118,22 @@ const brtGrijsWmtsLayer = getBRTALayer('brtachtergrondkaartgrijs')
 const brtPastelWmtsLayer = getBRTALayer('brtachtergrondkaartpastel')
 const brtWaterWmtsLayer = getBRTALayer('brtachtergrondkaartwater')
 const lufoLayer = getWmtsLayer('https://geodata.nationaalgeoregister.nl/luchtfoto/rgb/wmts', LUFO_ATTRIBUTION, 'Actueel_ortho25')
-brtGrijsWmtsLayer.set("visible", true)
+brtGrijsWmtsLayer.set('visible', true)
 
-const top10Layers = ["gebouwvlak","spoorbaandeellijn","waterdeelvlak","waterdeelvlakcontour","terreinvlakcontour","terreinvlak","wegdeelvlak","wegdeelvlakcontour"]
-const top10nlLayer = getWMSLayer('https://geodata.nationaalgeoregister.nl/top10nlv2/ows?', '', top10Layers, [])
-top10nlLayer.set("title", "Top10NL WMS")
+const top10Layers = ['gebouwvlak', 'spoorbaandeellijn', 'waterdeelvlak', 'waterdeelvlakcontour', 'terreinvlakcontour', 'terreinvlak', 'wegdeelvlak', 'wegdeelvlakcontour']
+const top10nlLayer = getWMSLayer('https://geodata.nationaalgeoregister.nl/top10nlv2/ows?', '',
+  top10Layers, [])
+top10nlLayer.set('title', 'Top10NL WMS')
+top10nlLayer.set('visible', false)
 
-// wegdeel_vlak_contour,wegdeel_vlak,terrein_vlak,terrein_vlak_contour,waterdeel_vlak_contour,waterdeel_vlak,spoorbaandeel_lijn,gebouw_vlak
+brtWmtsLayer.set("base", true)
+brtGrijsWmtsLayer.set("base", true)
+brtPastelWmtsLayer.set("base", true)
+brtWaterWmtsLayer.set("base", true)
+lufoLayer.set("base", true)
+top10nlLayer.set("base", true)
 
-// https://geodata.nationaalgeoregister.nl/top10nlv2/ows?SERVICE=WMS&
-
-
-const map = new Map({
+const MAP = new Map({
   layers: [
     top10nlLayer,
     brtWaterWmtsLayer,
@@ -146,61 +142,57 @@ const map = new Map({
     brtWmtsLayer,
     lufoLayer
   ],
-  target: document.getElementById("map"),
+  target: document.getElementById('map'),
   view: new View({
-    center: transform([5.43, 52.18], "EPSG:4326", 'EPSG:28992'),
+    center: transform([5.43, 52.18], 'EPSG:4326', 'EPSG:28992'),
     zoom: 8,
     projection: 'EPSG:28992'
   })
 })
 
-
-var layerSwitcher = new LayerSwitcher({
+var LAYERSWITCHER = new LayerSwitcher({
   tipLabel: 'Legend', // Optional label for button
   groupSelectStyle: 'none' // Can be 'children' [default], 'group' or 'none'
 })
-map.addControl(layerSwitcher)
-
-
-
+MAP.addControl(LAYERSWITCHER)
 
 document.getElementById('export-png').addEventListener('click', function () {
-  map.once('rendercomplete', function () {
-    var mapCanvas = document.createElement('canvas');
-    var size = map.getSize();
-    mapCanvas.width = size[0];
-    mapCanvas.height = size[1];
-    var mapContext = mapCanvas.getContext('2d');
+  MAP.once('rendercomplete', function () {
+    var mapCanvas = document.createElement('canvas')
+    var size = MAP.getSize()
+    mapCanvas.width = size[0]
+    mapCanvas.height = size[1]
+    var mapContext = mapCanvas.getContext('2d')
     Array.prototype.forEach.call(document.querySelectorAll('.ol-layer canvas'), function (canvas) {
       if (canvas.width > 0) {
-        var opacity = canvas.parentNode.style.opacity;
-        mapContext.globalAlpha = opacity === '' ? 1 : Number(opacity);
-        var transform = canvas.style.transform;
+        var opacity = canvas.parentNode.style.opacity
+        mapContext.globalAlpha = opacity === '' ? 1 : Number(opacity)
+        var transform = canvas.style.transform
         // Get the transform parameters from the style's transform matrix
-        var matrix = transform.match(/^matrix\(([^\(]*)\)$/)[1].split(',').map(Number);
+        var matrix = transform.match(/^matrix\(([^(]*)\)$/)[1].split(',').map(Number)
         // Apply the transform to the export map context
-        CanvasRenderingContext2D.prototype.setTransform.apply(mapContext, matrix);
-        mapContext.drawImage(canvas, 0, 0);
+        CanvasRenderingContext2D.prototype.setTransform.apply(mapContext, matrix)
+        mapContext.drawImage(canvas, 0, 0)
       }
-    });
+    })
     if (navigator.msSaveBlob) {
       // link download attribuute does not work on MS browsers
-      navigator.msSaveBlob(mapCanvas.msToBlob(), 'map.png');
+      navigator.msSaveBlob(mapCanvas.msToBlob(), 'map.png')
     } else {
-      var link = document.getElementById('image-download');
-      link.href = mapCanvas.toDataURL("image/png", 1);
-      link.click();
+      var link = document.getElementById('image-download')
+      link.href = mapCanvas.toDataURL('image/png', 1)
+      link.click()
     }
-  });
-  map.renderSync();
-});
+  })
+  MAP.renderSync()
+})
 
-function unpackLayers(capObj, result) {
+function unpackLayers (capObj, result) {
   if (!Array.isArray(capObj)) {
     capObj = [capObj]
   }
   capObj.forEach(function (lyr) {
-    if ("Layer" in lyr) {
+    if ('Layer' in lyr) {
       unpackLayers(lyr.Layer, result)
     } else {
       result.push(lyr)
@@ -209,44 +201,36 @@ function unpackLayers(capObj, result) {
   return result
 }
 
-function urlChanged() {
-  let getMapUrlEl = document.getElementById("GetMapUrl")
-  serviceUrl = getMapUrlEl.value
-
+function urlChanged () {
+  const getMapUrlEl = document.getElementById('GetMapUrl')
+  SERVICE_URL = getMapUrlEl.value
   const parser = new WMSCapabilities()
-  fetch(serviceUrl).then(function (response) {
+  fetch(SERVICE_URL).then(function (response) {
     return response.text()
   }).then(function (text) {
     var result = parser.read(text)
-    serviceTitle = result["Service"]["Title"]
+    SERVICE_TITLE = result.Service.Title
     let layers = []
     layers = unpackLayers(result.Capability, layers)
-    let layerNames = []
-
-    var tbl = document.getElementById("layerlist")
-    tbl.innerHTML = ""
-    var tblBody = document.createElement("tbody")
-
+    const layerNames = []
+    var tbl = document.getElementById('layerlist')
+    tbl.innerHTML = ''
+    var tblBody = document.createElement('tbody')
     layers.forEach(function (x) {
-      layerNames.push(x["Name"])
-      var row = document.createElement("tr")
-      row.setAttribute("draggable", "true")
-      row.classList.add("draggable")
-      
+      layerNames.push(x.Name)
+      var row = document.createElement('tr')
+      row.setAttribute('draggable', 'true')
+      row.classList.add('draggable')
       row.addEventListener('dragover', dragOver)
-      row.addEventListener("dragstart", dragStart)
-
-      var cell = document.createElement("td")
-      var cellText = document.createTextNode(x["Name"])
+      row.addEventListener('dragstart', dragStart)
+      var cell = document.createElement('td')
+      var cellText = document.createTextNode(x.Name)
       var checkbox = document.createElement('input')
-      var cell2 = document.createElement("td")
-      var cell3 = document.createElement("td")
-
-      var selectList = document.createElement("select");
-      selectList.setAttribute("layer", x["Name"])
-      selectList.classList.add("styleselect")
-      selectList.id = "mySelect";
-
+      var cell2 = document.createElement('td')
+      var cell3 = document.createElement('td')
+      var selectList = document.createElement('select')
+      selectList.setAttribute('layer', x.Name)
+      selectList.classList.add('styleselect')
       if (x.Style) {
         x.Style.forEach(function (style) {
           let title = style.Title
@@ -256,24 +240,22 @@ function urlChanged() {
               title = style.Name.split(':')[1]
             }
           }
-          let option = document.createElement("option")
+          const option = document.createElement('option')
           option.value = style.Name
           option.text = title
-          selectList.appendChild(option);
-        })}
-
-        selectList.addEventListener("change", function () {
-          switchLayer(serviceUrl, serviceTitle)
+          selectList.appendChild(option)
         })
-
-      cell3.appendChild(selectList);
-
-      checkbox.type = "checkbox"
+      }
+      selectList.addEventListener('change', function () {
+        switchLayer(SERVICE_URL, SERVICE_TITLE)
+      })
+      cell3.appendChild(selectList)
+      checkbox.type = 'checkbox'
       checkbox.checked = true
-      checkbox.classList.add("layerCheck")
-      checkbox.setAttribute("layer", x["Name"])
-      checkbox.addEventListener("change", function () {
-        switchLayer(serviceUrl, serviceTitle)
+      checkbox.classList.add('layerCheck')
+      checkbox.setAttribute('layer', x.Name)
+      checkbox.addEventListener('change', function () {
+        switchLayer(SERVICE_URL, SERVICE_TITLE)
       })
       cell2.appendChild(checkbox)
       cell.appendChild(cellText)
@@ -283,47 +265,88 @@ function urlChanged() {
       tblBody.appendChild(row)
     })
     tbl.appendChild(tblBody)
-    switchLayer(serviceUrl, serviceTitle)
+    switchLayer(SERVICE_URL, SERVICE_TITLE)
   })
 }
 
-var serviceUrl
-var serviceTitle
+var SERVICE_URL
+var SERVICE_TITLE
 
-var getMapUrlEl = document.getElementById("GetMapUrl")
-getMapUrlEl.addEventListener('blur', urlChanged)
+var GET_MAP_URL_EL = document.getElementById('GetMapUrl')
+GET_MAP_URL_EL.addEventListener('blur', urlChanged)
 
-let slEl = document.getElementById("opacitySlider")
-slEl.addEventListener("change", function(e){
-  switchLayer(serviceUrl, serviceTitle)
+const SL_EL = document.getElementById('opacitySlider')
+SL_EL.addEventListener('change', function (e) {
+  switchLayer(SERVICE_URL, SERVICE_TITLE)
 })
-    // slDiv.appe
 
-
-
-var _el
-function dragOver(e) {
-  console.log("over")
-  let closestRow = e.target.closest("tr")
-  if (isBefore(_el, closestRow))
-  closestRow.parentNode.insertBefore(_el, closestRow);
-  else
-  closestRow.parentNode.insertBefore(_el, closestRow.nextSibling)
-  switchLayer(serviceUrl, serviceTitle)
+var DRAG_EL
+function dragOver (e) {
+  const closestRow = e.target.closest('tr')
+  if (isBefore(DRAG_EL, closestRow)) { closestRow.parentNode.insertBefore(DRAG_EL, closestRow) } else { closestRow.parentNode.insertBefore(DRAG_EL, closestRow.nextSibling) }
+  switchLayer(SERVICE_URL, SERVICE_TITLE)
 }
 
-function dragStart(e) {
-  console.log("start")
-  e.dataTransfer.effectAllowed = "move";
-  e.dataTransfer.setData("text/plain", null); // Thanks to bqlou for their comment.
-  _el = e.target;
+function dragStart (e) {
+  e.dataTransfer.effectAllowed = 'move'
+  e.dataTransfer.setData('text/plain', null) // Thanks to bqlou for their comment.
+  DRAG_EL = e.target
 }
 
-function isBefore(el1, el2) {
-  if (el2.parentNode === el1.parentNode)
-    for (var cur = el1.previousSibling; cur && cur.nodeType !== 9; cur = cur.previousSibling)
-      if (cur === el2)
-        return true;
-  return false;
+function isBefore (el1, el2) {
+  if (el2.parentNode === el1.parentNode) {
+    for (var cur = el1.previousSibling; cur && cur.nodeType !== 9; cur = cur.previousSibling) {
+      if (cur === el2) { return true }
+    }
+  }
+  return false
 }
 
+const SIZE_SELECT_EL = document.getElementById('sizeSelect')
+
+SIZE_SELECT_EL.addEventListener('change', function (e) {
+  const x = e.target.value.split('x')[0]
+  const y = e.target.value.split('x')[1]
+  const mapEl = document.getElementById('map')
+  mapEl.style.width = `${x}px`
+  mapEl.style.height = `${y}px`
+  MAP.renderSync()
+  MAP.updateSize()
+})
+
+
+let grayscaleCheckbox = document.getElementById("grayscaleBaselayers")
+
+
+function postRender(evt){
+  evt.context.globalCompositeOperation = "color";
+  if (evt.context.globalCompositeOperation === "color") {
+    // operation is supported by browser
+    evt.context.fillStyle = "rgba(255,255,255," + GRAYSCALE / 100 + ")";
+    evt.context.fillRect(
+      0,
+      0,
+      evt.context.canvas.width,
+      evt.context.canvas.height
+    );
+  }
+  evt.context.globalCompositeOperation = "source-over";
+}
+
+var GRAYSCALE = grayscaleCheckbox.checked === true ? 100:0
+grayscaleCheckbox.addEventListener("change", function(e){
+  GRAYSCALE =  e.target.checked === true ? 100 : 0
+  MAP.render()
+})
+
+
+MAP
+  .getLayers().forEach(function(lyr){
+    if (lyr.get("base")){
+      // see comment on this answer https://stackoverflow.com/a/59819793
+      lyr.on("postrender", postRender)
+    }
+  }
+)
+
+  
